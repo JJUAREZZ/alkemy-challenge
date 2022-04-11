ea8980c8f1b0bb8e4427bdbd90e446f40d178ab3
JJUAREZZ <joacojuarez1@gmail.com>
2022-04-11 18:38:04 -0300

    sequelize config

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,20 +1,21 @@
-const ExpressServer = require('./server/expressServer');
-const mongooseLoader = require('./mongoose');
-const config = require('../config');
-const logger = require('./logger');
+const ExpressServer = require("./server/expressServer");
+const sequelize = require("./sequelize");
+const config = require("../config");
+const logger = require("./logger");
 
 module.exports = async () => {
-
-    await mongooseLoader();
-    logger.info('DB loaded and connected');
-    
+  try {
+    await sequelize.authenticate();
+    logger.info("DB loaded and connected");
     const server = new ExpressServer();
-    logger.info('Express Loaded');
+    logger.info("Express Loaded");
 
     server.start();
     logger.info(`#######################################
-      Server listening on port: ${config.port}
-      #######################################
-    `);
-
-}
+        Server listening on port: ${config.port}
+        #######################################
+      `);
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
